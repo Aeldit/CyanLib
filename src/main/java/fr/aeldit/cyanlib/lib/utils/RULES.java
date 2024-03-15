a7d88e8bff43a55c6c97f252e42805e90fb3f27c
aeldit <raphael.roger.92@gmail.com>
2024-03-15 11:44:40 +0100

    Remove the copyrights notices as there is already the license

--- src/main/java/fr/aeldit/cyanlib/lib/utils/RULES.java
+++ src/main/java/fr/aeldit/cyanlib/lib/utils/RULES.java
@@ -1,24 +1,8 @@
-/*
- * Copyright (c) 2023  -  Made by Aeldit
- *
- *              GNU LESSER GENERAL PUBLIC LICENSE
- *                  Version 3, 29 June 2007
- *
- *  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
- *  Everyone is permitted to copy and distribute verbatim copies
- *  of this license document, but changing it is not allowed.
- *
- *
- * This version of the GNU Lesser General Public License incorporates
- * the terms and conditions of version 3 of the GNU General Public
- * License, supplemented by the additional permissions listed in the LICENSE.txt file
- * in the repo of this mod (https://github.com/Aeldit/CyanLib)
- */
-
 package fr.aeldit.cyanlib.lib.utils;
 
 /**
- * {@link #NONE} is used to indicate that the option has no rule (you should not use it. If you want an option to have no rule, simply don't specify one)
+ * {@link #NONE} is used to indicate that the option has no rule (you should not use it. If you want an option to
+ * have no rule, simply don't specify one)
  * <p>
  * {@link #MAX_VALUE} indicates a maximum value
  * <p>
@@ -30,9 +14,11 @@ package fr.aeldit.cyanlib.lib.utils;
  * <p>
  * {@link #NEGATIVE_VALUE} indicates that the option must be negative and != 0
  * <p>
- * {@link #RANGE} indicates that the option can be between 2 values, which must both be specified in the option definition
+ * {@link #RANGE} indicates that the option can be between 2 values, which must both be specified in the option
+ * definition
  * <p>
- * {@link #LOAD_CUSTOM_TRANSLATIONS} can be held by only 1 option and indicates the code that when true, the custom translations will be loaded and used
+ * {@link #LOAD_CUSTOM_TRANSLATIONS} can be held by only 1 option and indicates the code that when true, the custom
+ * translations will be loaded and used
  */
 public enum RULES
 {
