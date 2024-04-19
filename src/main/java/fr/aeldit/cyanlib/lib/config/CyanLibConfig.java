ebf15e5fc195a85627d2549e86a4c8cbff564184
aeldit <raphael.roger.92@gmail.com>
2024-04-19 21:01:25 +0200

    Add some doc

--- src/main/java/fr/aeldit/cyanlib/lib/config/CyanLibConfig.java
+++ src/main/java/fr/aeldit/cyanlib/lib/config/CyanLibConfig.java
@@ -4,5 +4,11 @@ import java.util.Map;
 
 public interface CyanLibConfig
 {
+    /**
+     * This is used because the mod being potentially server-side, the clients playing on the server wouldn't have
+     * the translations
+     *
+     * @return The HashMap containing all the translations for your mod
+     */
     Map<String, String> getDefaultTranslations();
 }
