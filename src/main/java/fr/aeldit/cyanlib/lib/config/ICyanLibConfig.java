9fb3436b794a224e38afc3b2e073332fa5da2d24
aeldit <raphael.roger.92@gmail.com>
2024-08-14 15:52:14 +0200

    Update doc

--- src/main/java/fr/aeldit/cyanlib/lib/config/ICyanLibConfig.java
+++ src/main/java/fr/aeldit/cyanlib/lib/config/ICyanLibConfig.java
@@ -5,6 +5,11 @@ import java.util.Map;
 public interface ICyanLibConfig
 {
     /**
+     * This function's purpose is to store and return the mod's default translations. We don't use the traditional way,
+     * because with the traditional way requires the client to have the translations loaded (either by a resource pack
+     * or by the mod). By storing the translations in json files in the server's config and here in the code,
+     * we can guarantee that the client users won't need to have the translations loaded.
+     * <p>
      * This is used because the mod being potentially server-side, the clients playing on the server wouldn't have
      * the translations
      *
